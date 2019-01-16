47eba324659a1d7f307f58d15bf0d214af889ed9
kubarium <iam@denizkumsal.com>
2019-01-16 00:11:48 -0500

    extracts material and level information from description

--- src/data/cruncher.js
+++ src/data/cruncher.js
@@ -27,31 +27,54 @@ function jsonize() {
       "Class"
     ];
 
-    //const spellFile = `${file.toLocaleLowerCase()}.csv`;
-
-    //var content = fs.readFileSync(path.resolve(spellFile), { encoding: "utf8" });
-    /* 
-  var hasHeader = content.split("\n")[0] == header;
-
-  if (!hasHeader) {
-    fs.writeFileSync(spellFile, `${header}\n${content}`);
-  } */
-
-    //console.log(`${header}\n${content}`);
     console.clear();
-    var jsonized = csv({
-      noheader: true,
-      headers,
-      delimiter: ";"
-    })
-      .fromFile(`${file.toLocaleLowerCase()}.csv`)
-      .then(jsonObj => {
-        fs.writeFileSync(
-          `${file.toLocaleLowerCase()}.json`,
-          JSON.stringify(jsonObj)
-        );
-      });
+    var jsonized = csv({noheader: true, headers, checkType: true, delimiter: ";"}).fromFile(`${file.toLocaleLowerCase()}.csv`)
+      .then(materialize)
+      .then(level)
+      .then(jsonized => fs.writeFileSync(`${file.toLocaleLowerCase()}.json`, JSON.stringify(jsonized)));
   });
+}
+
+function materialize(jsonized) {
+  return new Promise(resolve => {
+    var materialPattern = /^(\(.*?\))/;
+
+    jsonized.forEach(item => {
+      if (RegExp(materialPattern, "g").test(item.Description)) {
+        var match = RegExp(materialPattern, "g").exec(item.Description)[0];
+
+        item.Material = match
+          .replace(")", "")
+          .replace("(", "");
+        item.Description = item
+          .Description
+          .replace(materialPattern, "");
+      }
+    })
+
+    resolve(jsonized)
+  })
+}
+function level(jsonized) {
+  return new Promise(resolve => {
+
+    var levelPattern = /(<br> <b>At Higher Levels<\/b>: .*)/;
+
+    jsonized.forEach(item => {
+
+      if (RegExp(levelPattern, "g").test(item.Description)) {
+        var match = RegExp(levelPattern, "g").exec(item.Description)[0];
+
+        item.HigherLevel = match.replace("<br> <b>At Higher Levels<\/b>: ", "");
+        item.Description = item
+          .Description
+          .replace(levelPattern, "");
+      }
+    })
+
+    resolve(jsonized)
+
+  })
 }
 
 jsonize();
